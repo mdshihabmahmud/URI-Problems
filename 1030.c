4c5c5516a82a5e9c040f07325be1b5c995d03085
Md. Shihab Mahmud <69903095+mdshihabmahmud@users.noreply.github.com>
2020-12-08 10:50:28 +0600

    Create 1030.c

---
+++ 1030.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int josephusLoop(int n, int k, int startingPoint)
+{
+ if(n == 1)
+  return 1;
+
+ int newSp, survivor;
+ newSp = (startingPoint + k - 2) % n + 1;
+  survivor = josephusLoop(n - 1, k, newSp);
+
+  if(survivor < newSp){
+   return survivor;
+  }else{
+   return survivor + 1;
+  }
+}
+
+int josephus(int n, int k)
+{
+ return josephusLoop(n, k, 1);
+}
+
+int main()
+{
+ int nc, n, k, s, i;
+ scanf("%i", &nc);
+
+ for (i = 1; i <= nc; ++i)
+ {
+  scanf("%i %i", &n, &k);
+  s = josephus(n, k);
+  printf("Case %i: %i\n", i, s);
+ }
+
+ return 0;
+}
