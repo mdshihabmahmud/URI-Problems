d7a44df2968e878698be707548b0f2bbbe11df10
Md. Shihab Mahmud <69903095+mdshihabmahmud@users.noreply.github.com>
2020-12-14 10:39:56 +0600

    Create 1074.py

---
+++ 1074.py
@@ -0,0 +1,9 @@
+l=[]
+a=int(input())
+for n in range(1,(a+1)):
+    l.append(int(input()))
+for n in l:
+    if (n%2!=0)and (n<0 and="" if="" n="" negative="" print="">0):
+        print("ODD POSITIVE")
+    if(n%2==0)and(n<0 and="" if="" n="" negative="" print="">0):
+        print("EVEN POSITIVE")
