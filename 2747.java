e2b1a00c87adc81ee4766085af4f6377b0d475b9
Md. Shihab Mahmud <69903095+mdshihabmahmud@users.noreply.github.com>
2021-01-09 19:14:37 +0600

    Create 2747.java

---
+++ 2747.java
@@ -0,0 +1,13 @@
+public class URI_2747 {
+ 
+    public static void main(String[] args) {
+        System.out.printf("---------------------------------------\n");
+        System.out.printf("|                                     |\n");
+        System.out.printf("|                                     |\n");
+        System.out.printf("|                                     |\n");
+        System.out.printf("|                                     |\n");
+        System.out.printf("|                                     |\n");
+        System.out.printf("---------------------------------------\n");
+    }
+ 
+}
