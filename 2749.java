514469822796a1317279f88d019f26919f38b231
Md. Shihab Mahmud <69903095+mdshihabmahmud@users.noreply.github.com>
2021-01-09 19:15:43 +0600

    Create 2749.java

---
+++ 2749.java
@@ -0,0 +1,15 @@
+public class URI_2749 {
+ 
+    public static void main(String[] args) {
+ 
+        System.out.printf("---------------------------------------\n");
+        System.out.printf("|x = 35                               |\n");
+        System.out.printf("|                                     |\n");
+        System.out.printf("|               x = 35                |\n");
+        System.out.printf("|                                     |\n");
+        System.out.printf("|                               x = 35|\n");
+        System.out.printf("---------------------------------------\n");
+ 
+    }
+ 
+}
