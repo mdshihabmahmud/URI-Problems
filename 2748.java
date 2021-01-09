816497c393138f1567840c8f08e3e27f348307f2
Md. Shihab Mahmud <69903095+mdshihabmahmud@users.noreply.github.com>
2021-01-09 19:15:06 +0600

    Create 2748.java

---
+++ 2748.java
@@ -0,0 +1,15 @@
+public class URI_2748 {
+ 
+    public static void main(String[] args) {
+ 
+    System.out.printf("---------------------------------------\n");
+    System.out.printf("|        Roberto                      |\n");
+    System.out.printf("|                                     |\n");
+    System.out.printf("|        5786                         |\n");
+    System.out.printf("|                                     |\n");
+    System.out.printf("|        UNIFEI                       |\n");
+    System.out.printf("---------------------------------------\n");
+ 
+    }
+ 
+}
